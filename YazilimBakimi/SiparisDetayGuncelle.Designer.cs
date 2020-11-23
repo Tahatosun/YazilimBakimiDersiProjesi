2a9af9715bcbc3a1a9536b373f5d1a122803a4ae
Tahatosun <35890316+Tahatosun@users.noreply.github.com>
2020-11-23 22:11:59 +0300

    SiparisGuncellemeEkrani Tasarlandı.

---
+++ YazilimBakimi/SiparisDetayGuncelle.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace YazilimBakimi
+{
+    partial class SiparisDetayGuncelle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSiparisGuncelleUrunID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSiparisUrunGuncelle = new MetroFramework.Controls.MetroTile();
+            this.txtToplamFiyatGuncelle = new System.Windows.Forms.TextBox();
+            this.txtSiparisUrunAdediGuncelle = new System.Windows.Forms.TextBox();
+            this.txtSiparisUrunAdiGuncelle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSiparisGuncelleUrunID
+            // 
+            this.txtSiparisGuncelleUrunID.Enabled = false;
+            this.txtSiparisGuncelleUrunID.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSiparisGuncelleUrunID.Location = new System.Drawing.Point(149, 123);
+            this.txtSiparisGuncelleUrunID.Name = "txtSiparisGuncelleUrunID";
+            this.txtSiparisGuncelleUrunID.Size = new System.Drawing.Size(171, 26);
+            this.txtSiparisGuncelleUrunID.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(23, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 21);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Ürün ID:";
+            // 
+            // btnSiparisUrunGuncelle
+            // 
+            this.btnSiparisUrunGuncelle.ActiveControl = null;
+            this.btnSiparisUrunGuncelle.Location = new System.Drawing.Point(27, 307);
+            this.btnSiparisUrunGuncelle.Name = "btnSiparisUrunGuncelle";
+            this.btnSiparisUrunGuncelle.Size = new System.Drawing.Size(293, 49);
+            this.btnSiparisUrunGuncelle.TabIndex = 27;
+            this.btnSiparisUrunGuncelle.Text = "Siparişe Ait Ürünü Güncelle";
+            this.btnSiparisUrunGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnSiparisUrunGuncelle.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
+            this.btnSiparisUrunGuncelle.UseSelectable = true;
+            // 
+            // txtToplamFiyatGuncelle
+            // 
+            this.txtToplamFiyatGuncelle.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtToplamFiyatGuncelle.Location = new System.Drawing.Point(149, 223);
+            this.txtToplamFiyatGuncelle.Name = "txtToplamFiyatGuncelle";
+            this.txtToplamFiyatGuncelle.Size = new System.Drawing.Size(171, 26);
+            this.txtToplamFiyatGuncelle.TabIndex = 26;
+            // 
+            // txtSiparisUrunAdediGuncelle
+            // 
+            this.txtSiparisUrunAdediGuncelle.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSiparisUrunAdediGuncelle.Location = new System.Drawing.Point(149, 192);
+            this.txtSiparisUrunAdediGuncelle.Name = "txtSiparisUrunAdediGuncelle";
+            this.txtSiparisUrunAdediGuncelle.Size = new System.Drawing.Size(171, 26);
+            this.txtSiparisUrunAdediGuncelle.TabIndex = 25;
+            // 
+            // txtSiparisUrunAdiGuncelle
+            // 
+            this.txtSiparisUrunAdiGuncelle.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSiparisUrunAdiGuncelle.Location = new System.Drawing.Point(149, 158);
+            this.txtSiparisUrunAdiGuncelle.Name = "txtSiparisUrunAdiGuncelle";
+            this.txtSiparisUrunAdiGuncelle.Size = new System.Drawing.Size(171, 26);
+            this.txtSiparisUrunAdiGuncelle.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(23, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 21);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Toplam Fiyat:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(23, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Ürün Adedi:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // SiparisDetayGuncelle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 441);
+            this.Controls.Add(this.txtSiparisGuncelleUrunID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSiparisUrunGuncelle);
+            this.Controls.Add(this.txtToplamFiyatGuncelle);
+            this.Controls.Add(this.txtSiparisUrunAdediGuncelle);
+            this.Controls.Add(this.txtSiparisUrunAdiGuncelle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "SiparisDetayGuncelle";
+            this.Text = "SiparisDetayGuncelle";
+            this.Load += new System.EventHandler(this.SiparisDetayGuncelle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSiparisGuncelleUrunID;
+        private System.Windows.Forms.Label label5;
+        private MetroFramework.Controls.MetroTile btnSiparisUrunGuncelle;
+        private System.Windows.Forms.TextBox txtToplamFiyatGuncelle;
+        private System.Windows.Forms.TextBox txtSiparisUrunAdediGuncelle;
+        private System.Windows.Forms.TextBox txtSiparisUrunAdiGuncelle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- YazilimBakimi/SiparisDetayiGoruntulemeEkrani.Designer.cs
+++ YazilimBakimi/SiparisDetayiGoruntulemeEkrani.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.grpBxSiparisDetayUrunleri = new System.Windows.Forms.GroupBox();
             this.dataGridSiparisDetayUrunler = new MetroFramework.Controls.MetroGrid();
             this.grpBxSiparisDetayGorGuncelle = new System.Windows.Forms.GroupBox();
@@ -38,9 +38,13 @@
             this.lblSiparisID = new System.Windows.Forms.Label();
             this.btnSiparistenUrunuSil = new MetroFramework.Controls.MetroTile();
             this.btnSiparisiGuncelle = new MetroFramework.Controls.MetroTile();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSiparisteGuncellenecekUrunID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.grpBxSiparisDetayUrunleri.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridSiparisDetayUrunler)).BeginInit();
             this.grpBxSiparisDetayGorGuncelle.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpBxSiparisDetayUrunleri
@@ -65,23 +69,23 @@
             this.dataGridSiparisDetayUrunler.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridSiparisDetayUrunler.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dataGridSiparisDetayUrunler.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridSiparisDetayUrunler.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridSiparisDetayUrunler.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridSiparisDetayUrunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridSiparisDetayUrunler.DefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridSiparisDetayUrunler.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridSiparisDetayUrunler.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridSiparisDetayUrunler.EnableHeadersVisualStyles = false;
             this.dataGridSiparisDetayUrunler.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
@@ -89,14 +93,14 @@
             this.dataGridSiparisDetayUrunler.Location = new System.Drawing.Point(3, 19);
             this.dataGridSiparisDetayUrunler.Name = "dataGridSiparisDetayUrunler";
             this.dataGridSiparisDetayUrunler.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridSiparisDetayUrunler.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridSiparisDetayUrunler.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridSiparisDetayUrunler.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dataGridSiparisDetayUrunler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridSiparisDetayUrunler.Size = new System.Drawing.Size(835, 433);
@@ -109,7 +113,7 @@
             this.grpBxSiparisDetayGorGuncelle.Controls.Add(this.txtSiparistenSilinecekUrunID);
             this.grpBxSiparisDetayGorGuncelle.Controls.Add(this.lblSiparisID);
             this.grpBxSiparisDetayGorGuncelle.Controls.Add(this.btnSiparistenUrunuSil);
-            this.grpBxSiparisDetayGorGuncelle.Location = new System.Drawing.Point(873, 176);
+            this.grpBxSiparisDetayGorGuncelle.Location = new System.Drawing.Point(873, 243);
             this.grpBxSiparisDetayGorGuncelle.Name = "grpBxSiparisDetayGorGuncelle";
             this.grpBxSiparisDetayGorGuncelle.Size = new System.Drawing.Size(214, 112);
             this.grpBxSiparisDetayGorGuncelle.TabIndex = 17;
@@ -148,9 +152,9 @@
             // btnSiparisiGuncelle
             // 
             this.btnSiparisiGuncelle.ActiveControl = null;
-            this.btnSiparisiGuncelle.Location = new System.Drawing.Point(873, 107);
+            this.btnSiparisiGuncelle.Location = new System.Drawing.Point(10, 57);
             this.btnSiparisiGuncelle.Name = "btnSiparisiGuncelle";
-            this.btnSiparisiGuncelle.Size = new System.Drawing.Size(214, 49);
+            this.btnSiparisiGuncelle.Size = new System.Drawing.Size(196, 49);
             this.btnSiparisiGuncelle.TabIndex = 16;
             this.btnSiparisiGuncelle.Text = "Güncelle";
             this.btnSiparisiGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -158,13 +162,42 @@
             this.btnSiparisiGuncelle.UseSelectable = true;
             this.btnSiparisiGuncelle.Click += new System.EventHandler(this.btnSiparisiGuncelle_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtSiparisteGuncellenecekUrunID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnSiparisiGuncelle);
+            this.groupBox1.Location = new System.Drawing.Point(873, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 112);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÜRÜNÜ SİPARİŞTEN SİL";
+            // 
+            // txtSiparisteGuncellenecekUrunID
+            // 
+            this.txtSiparisteGuncellenecekUrunID.Location = new System.Drawing.Point(59, 31);
+            this.txtSiparisteGuncellenecekUrunID.Name = "txtSiparisteGuncellenecekUrunID";
+            this.txtSiparisteGuncellenecekUrunID.Size = new System.Drawing.Size(147, 20);
+            this.txtSiparisteGuncellenecekUrunID.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ÜRÜN ID:";
+            // 
             // SiparisDetayiGoruntulemeEkrani
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1129, 572);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grpBxSiparisDetayGorGuncelle);
-            this.Controls.Add(this.btnSiparisiGuncelle);
             this.Controls.Add(this.grpBxSiparisDetayUrunleri);
             this.Name = "SiparisDetayiGoruntulemeEkrani";
             this.Text = "SiparisDetayiGoruntulemeEkrani";
@@ -173,6 +206,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridSiparisDetayUrunler)).EndInit();
             this.grpBxSiparisDetayGorGuncelle.ResumeLayout(false);
             this.grpBxSiparisDetayGorGuncelle.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -186,5 +221,8 @@
         private System.Windows.Forms.Label lblSiparisID;
         private MetroFramework.Controls.MetroTile btnSiparistenUrunuSil;
         private MetroFramework.Controls.MetroTile btnSiparisiGuncelle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSiparisteGuncellenecekUrunID;
+        private System.Windows.Forms.Label label1;
     }
 }
